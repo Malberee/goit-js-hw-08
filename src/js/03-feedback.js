99477ec1d2be50d4ed5cdf111339491c1f9ceedd
Malberee <82259650+Malberee@users.noreply.github.com>
2023-03-11 14:23:27 +0200

    fixed feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,14 +2,15 @@ import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
 
-const dataForm = {};
-
 formRef.addEventListener('input', throttle(onInput, 500));
 formRef.addEventListener('submit', onSubmit);
 
+let dataForm = {};
+
 if (localStorage.getItem('feedback-form-state')) {
   loadSavedInput();
-}
+  dataForm = JSON.parse(localStorage.getItem('feedback-form-state'))
+} 
 
 function onInput(e) {
   dataForm[e.target.name] = e.target.value;
